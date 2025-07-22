dce97eeb2491e8cab238811b17443de1aeb20e84
vickyvillalba22 <villalbavictoria120@gmail.com>
2025-07-22 17:33:43 -0300

    martes 22/7

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,4 +92,155 @@ function armarCategorias(){
 
 armarCategorias();
 
+/*SKILLS*/
+
+const softs = [
+    {name: "Communication", icon: "bi bi-chat-left-text"},
+    {name: "Teamwork", icon: "bi bi-people"},
+    {name: "Problem Solving", icon: "bi bi-lightbulb"},
+    {name: "Adaptability", icon: "bi bi-arrows-move"},
+    {name: "Creativity", icon: "bi bi-brush"},
+    {name: "Time Management", icon: "bi bi-clock"}
+]
+
+const contenedorSofts = document.getElementById("softSkills");
+
+function armarSoft(){
+
+    for(let i=0; i<softs.length; i++){
+
+        let boxSkill = document.createElement("div");
+        boxSkill.classList.add("df", "centerY", "w50", "boxSkill");
+
+        boxSkill.innerHTML = `
+            <i class="${softs[i].icon} blanco"></i>
+            <h4 class="blanco fuente">${softs[i].name}</h4>
+        `;
+
+        contenedorSofts.appendChild(boxSkill);
+
+    }
+
+}
+
+armarSoft();
+
+const hardSkills = [
+    {
+        nombre: "Web Development",
+        herramientas: [
+            { nombre: "HTML", nivel: 5 },
+            { nombre: "CSS", nivel: 5 },
+            { nombre: "JavaScript", nivel: 4 },
+            { nombre: "Python", nivel: 1 },
+            { nombre: "Scratch", nivel: 2 }
+        ]
+    },
+    {
+        nombre: "Graphic Design",
+        herramientas: [
+            { nombre: "Adobe Photoshop", nivel: 4 },
+            { nombre: "Adobe Illustrator", nivel: 4 },
+            { nombre: "Adobe Express", nivel: 4 },
+            { nombre: "Canva", nivel: 4 }
+        ]
+    },
+    {
+        nombre: "2D Animation",
+        herramientas: [
+            { nombre: "After Effects", nivel: 3 },
+            { nombre: "Animate", nivel: 3 }
+        ]
+    },
+    {
+        nombre: "Branding",
+        herramientas: [
+            { nombre: "Adobe Illustrator", nivel: 4 }
+        ]
+    },
+    {
+        nombre: "Audiovisual Production",
+        herramientas: [
+            { nombre: "Premiere Pro", nivel: 4 },
+            { nombre: "Lightroom", nivel: 4 },
+            { nombre: "Capcut", nivel: 4 },
+            { nombre: "Ableton", nivel: 2 }
+        ]
+    },
+    {
+        nombre: "UX/UI Design",
+        herramientas: [
+            { nombre: "Figma", nivel: 4 }
+        ]
+    }
+]
+
+const cajaHard = document.getElementById("hardSkills");
+
+function llenarHard(){
+
+        for (let i = 0; i < hardSkills.length; i++) {
+                
+            let cont = document.createElement("div");
+            cont.classList.add("borde-bottom", "df", "cajita-header");
+            cont.innerHTML = `
+                <i class="bi bi-arrow-right"></i>
+                <h4 class="item-header negro">${hardSkills[i].nombre}</h4>
+            `;
+
+            cajaHard.appendChild(cont);
+        }
+
+}
+
+llenarHard();
+
+/*FOOTER*/
+
+const datosFooter = [
+    {
+        icon: "bi bi-envelope",
+        dato: "villalbavictoria120@gmail.com",
+        enlace: "mailto:villalbavictoria120@gmail.com"
+    },
+    {
+        icon: "bi bi-whatsapp",
+        dato: "+54 291 5043 053",
+        enlace: "https://wa.me/542915043053"
+    },
+    {
+        icon: "bi bi-linkedin",
+        dato: "Victoria Villalba",
+        enlace: "https://www.linkedin.com/in/vvillalba/"
+    },
+    {
+        icon: "bi bi-github",
+        dato: "vickyvillalba22",
+        enlace: "https://github.com/vickyvillalba22"
+    }
+]
+
+const cajaFooter = document.getElementById("contactos");
+
+function llenarFooter() {
+    
+    for(let i = 0; i < datosFooter.length; i++) {
+
+        let cont = document.createElement("a");
+        cont.setAttribute("href", datosFooter[i].enlace);
+        cont.setAttribute("target", "_blank");
+        cont.classList.add("df", "centerY", "caja-footer");
+
+        cont.innerHTML = `
+            <i class="${datosFooter[i].icon} blanco"></i>
+            <p class="blanco">${datosFooter[i].dato}</p>
+        `;
+
+        cajaFooter.appendChild(cont);
+    }
+
+}
+
+llenarFooter();
+
 
